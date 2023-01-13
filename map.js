945c6b9557c5a62e7343211558884a1343ab1023
Andrei Ciudacov <stauceni.android@gmail.com>
2023-01-13 20:06:35 +0200

    remove debug line

--- map.js
+++ map.js
@@ -64,7 +64,6 @@ function buttonClick() {
         let miles = document.getElementById('miles').value;
         let placeName = place.address_components.find(a => a.types.includes('locality')).short_name;
         let placeState = place.address_components.find(a => a.types.includes('administrative_area_level_1')).short_name;
-        window.alert("DEBUG: " + placeName + ", " + placeState);
-        Telegram.WebApp.sendData(place.address_components[0].short_name + "+" + place.address_components[2].short_name + "+" + place.place_id + "+" + miles);
+        Telegram.WebApp.sendData(placeName + "+" + placeState + "+" + place.place_id + "+" + miles);
     }
 }
